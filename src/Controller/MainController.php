1fd0f42e2a72923d71b59981852b62608df67313
Ignatious <ignatiousr80@gmail.com>
2025-05-24 16:30:45 +0600

    update-1

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\StarshipRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-class MainController{
-
+class MainController extends AbstractController 
+{
     #[Route('/')]
     
-    public function homepage()
+    public function homepage(StarshipRepository $starshipRepository): Response
     {
-        return new Response('<strong>Hello Symfony</strong>');
+        $ships = $starshipRepository->findAll();
+        $starship =  $ships[array_rand($ships)];
+        //$starshipCount = count($ships);
+        //return new Response('<strong>Hello Symfony</strong>');
+        return $this->render('main/homepage.html.twig', ['numberOfStarships' => $ships, 'starship'=> $starship]);
     }
 }
